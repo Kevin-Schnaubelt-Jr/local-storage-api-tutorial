9135063ba461c8fb4eb1cef421101c4bf32c3b26
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-07-24 14:08:41 -0400

    upgrade

--- main.js
+++ main.js
@@ -34,6 +34,23 @@ let gameState = JSON.parse(localStorage.getItem('gameState')) || {
             baseReturn: 0.1,
             doubles: 0,
             upgradeCost: 0,
+            currentFinger: "Cursor",
+            fingers: {
+                "Cursor":0,
+                "Thousand": 0.1,
+                "Million": 0.5,
+                "Billion": 5,
+                "Trillion": 100,
+                "Quadrillion": 2000,
+                "Quintillion": 40000,
+                "Sextillion": 800000,
+                "Septillion": 16000000,
+                "Octillion": 320000000,
+                "Nonillion": 6400000000,
+                "Decillion": 128000000000,
+                "Undecillion": 2560000000000,
+
+            }
         },
         Grandma: {
             name: 'Grandma',
@@ -42,6 +59,8 @@ let gameState = JSON.parse(localStorage.getItem('gameState')) || {
             baseReturn: 1,
             doubles: 0,
             upgradeCost: 0,
+            upgradeGrandmaCost: 0,
+            upgradeGrandmaType: 'Farm',
         },
         Farm: {
             name: 'Farm',
@@ -62,13 +81,14 @@ let globalBoost = 1;
 let totalcps = 0;
 let prodUpgradeEfficiency = 0;
 let grandmaTotal = 0;
+let nonCursorBuildingTotal = 0;
 
 window.onload = function() {
     GoActionGo();
     addEventListeners();
     // Other onload tasks...
-    console.log(adjustedBuildings);
-    console.log(gameState);
+    console.log("adjusted", adjustedBuildings);
+    console.log("gamestate", gameState);
 
 };
 
@@ -94,7 +114,12 @@ function CalcGlobalBoost() {
     globalBoost = flavors * kittenBoost;
 
     grandmaTotal = gameState.buildings.Grandma.amount;
-    console.log(grandmaTotal);
+    for (let building in gameState.buildings) {
+        if (building !== 'Cursor') {
+            nonCursorBuildingTotal += gameState.buildings[building].amount;
+        }
+    }
+    console.log("non cursor total", nonCursorBuildingTotal);
 }
 
 function AdjustBuildings() {
@@ -104,9 +129,21 @@ function AdjustBuildings() {
         let baseReturn = gameState.buildings[building].baseReturn;
         let doubles = gameState.buildings[building].doubles;
         let upgradeCost = gameState.buildings[building].upgradeCost;
-        let grandmaBoosted = gameState.buildings[building].grandmaBoosted ? gameState.buildings[building].grandmaBoosted : false;
-        let grandmaBoost = gameState.buildings[building].grandmaBoost ? gameState.buildings[building].grandmaBoost : 0;
-        let cps = baseReturn * (doubles ? 2**doubles : 1) * (gameState.buildings[building].grandmaBoosted ? 1 + 0.01 * (grandmaTotal/grandmaBoost) : 1) * globalBoost;
+
+        let grandmaBoosted = gameState.buildings[building].grandmaBoosted ?? false;
+        let grandmaBoost = gameState.buildings[building].grandmaBoost ?? null;
+
+        let currentFinger = gameState.buildings[building].currentFinger ?? false;
+        console.log("current finger", currentFinger);
+        let fingerBoost = currentFinger ? gameState.buildings[building].fingers[currentFinger] : false;
+        console.log("finger boost", fingerBoost);
+        console.log("finger total", currentFinger ? fingerBoost * nonCursorBuildingTotal : 0);
+
+        let cps = ((baseReturn * (doubles ? 2**doubles : 1) * (grandmaBoosted ? 1 + 0.01 * (grandmaTotal/grandmaBoost) : 1)) + (currentFinger ? fingerBoost * nonCursorBuildingTotal : 0)) * globalBoost;
+
+        let upgradeGrandmaCost = gameState.buildings[building].upgradeGrandmaCost ?? null;
+        let upgradeGrandmaType = gameState.buildings[building].upgradeGrandmaType ?? null;
+
 
         adjustedBuildings[building] = {
             name: building,
@@ -115,8 +152,23 @@ function AdjustBuildings() {
             totalCost: amount ? baseCost * 1.15**amount : baseCost,
             cps: cps,
             upgradeCost: upgradeCost,
-            grandmaBoosted: grandmaBoosted,
         }
+
+        if (currentFinger) {
+            adjustedBuildings[building].currentFinger = currentFinger;
+        }
+
+        if (grandmaBoost !== null) {
+            adjustedBuildings[building].grandmaBoosted = grandmaBoosted;
+            adjustedBuildings[building].grandmaBoost = grandmaBoost;
+        }
+
+         // If upgradeGrandmaCost is not null, add it to the object.
+        if (upgradeGrandmaCost !== null) {
+            adjustedBuildings[building].upgradeGrandmaCost = upgradeGrandmaCost;
+            adjustedBuildings[building].upgradeGrandmaType = upgradeGrandmaType;
+        }
+
         // Add building's cps to totalcps
         totalcps += cps * amount;
     }
@@ -130,10 +182,17 @@ function CalculateBuyEfficiency() {
         let amount = adjustedBuildings[building].amount;
         let upgradeCost = adjustedBuildings[building].upgradeCost;
 
+        let upgradeGrandmaCost = adjustedBuildings[building].upgradeGrandmaCost ?? null;
+        let upgradeGrandmaType = adjustedBuildings[building].upgradeGrandmaType ?? null;
+
         adjustedBuildings[building].buyEfficiency = BuyEfficiency(cost, totalcps, cpsIncrease);
 
         if (upgradeCost) {
             adjustedBuildings[building].upgradeEfficiency = UpgradeEfficiency(upgradeCost, totalcps, cpsIncrease*amount);
+        }
+
+        if (upgradeGrandmaCost !== null) {
+            adjustedBuildings[building].upgradeGrandmaEfficiency = UpgradeGrandmaEfficiency(upgradeGrandmaCost, cpsIncrease, upgradeGrandmaType, totalcps);
         }
 
     }
@@ -148,6 +207,15 @@ function BuyEfficiency(cost, totalcps, cpsIncrease){
 function UpgradeEfficiency(cost, totalcps, cpsIncrease) {
     let waitTime = totalcps ? cost / totalcps : 0;
     let efficiency = cost / cpsIncrease;
+    return waitTime + efficiency;
+}
+
+function UpgradeGrandmaEfficiency(upgradeGrandmaCost ,grandmaCps, upgradeGrandmaType, totalcps) {
+    let buildingTotal = adjustedBuildings[upgradeGrandmaType].amount * adjustedBuildings[upgradeGrandmaType].cps;
+    let buildingGrandmaBoost = adjustedBuildings[upgradeGrandmaType].grandmaBoost;
+    let grandmas = adjustedBuildings.Grandma.amount / buildingGrandmaBoost;
+    let waitTime = totalcps ? upgradeGrandmaCost / totalcps : 0;
+    let efficiency = upgradeGrandmaCost / (grandmaCps + (buildingTotal * (1 + 0.01 * grandmas) - buildingTotal));
     return waitTime + efficiency;
 }
 
@@ -168,16 +236,23 @@ function Display() {
     for (let buildingKey in adjustedBuildings) {
         let building = adjustedBuildings[buildingKey];
 
+        if (building.name === 'Cursor') {
+            document.getElementById('finger-names').value = building.currentFinger;
+        }
+
         document.getElementById(`${building.name.toLowerCase()}Number`).value = building.amount;
         if (document.getElementById(`${building.name.toLowerCase()}GrandmaBoost`)) {
-            console.log(building.grandmaBoosted);
             document.getElementById(`${building.name.toLowerCase()}GrandmaBoost`).checked = building.grandmaBoosted;
         }
         document.getElementById(`${building.name.toLowerCase()}Doubles`).value = building.doubles;
         document.getElementById(`${building.name.toLowerCase()}Cost`).innerHTML = formatValue(building.totalCost, 2, true);
         document.getElementById(`${building.name.toLowerCase()}CpS`).innerHTML = formatValue(building.cps, 1);
-        document.getElementById(`${building.name.toLowerCase()}Buy`).innerHTML = formatValue(building.buyEfficiency, 0);
-        document.getElementById(`${building.name.toLowerCase()}Upgrade`).placeholder = building.upgradeEfficiency ? formatValue(building.upgradeEfficiency, 0) : 'Upgrade';
+        document.getElementById(`${building.name.toLowerCase()}Buy`).innerHTML = formatValue(building.buyEfficiency, 2, true);
+        document.getElementById(`${building.name.toLowerCase()}Upgrade`).placeholder = building.upgradeEfficiency ? formatValue(building.upgradeEfficiency, 2) : 'Upgrade';
+        if (document.getElementById(`${building.name.toLowerCase()}GrandmaUpgrade`)) {
+            document.getElementById(`${building.name.toLowerCase()}GrandmaUpgrade`).placeholder = building.upgradeGrandmaEfficiency ? formatValue(building.upgradeGrandmaEfficiency, 2) : 'Upgrade';
+            document.getElementById('building-names').value = building.upgradeGrandmaType;
+        }
     }
 
     document.getElementById('totalCpS').innerHTML = `Total CpS: ${formatValue(totalcps, 1)}`;
@@ -216,6 +291,45 @@ function Display() {
 
 function addEventListeners() {
     // For the buildings
+    BuildingsEvent();
+
+    // For the finger
+    // Get the select element by id
+    let fingerNamesSelect = document.getElementById('finger-names');
+
+    // Add event listener for 'change' event
+    fingerNamesSelect.addEventListener('change', () => {
+        // Get selected option value
+        let selectedFinger = fingerNamesSelect.value;
+
+        // Update currentFinger in gameState for Cursor
+        gameState.buildings['Cursor'].currentFinger = selectedFinger;
+
+        // Save to local storage
+        localStorage.setItem('gameState', JSON.stringify(gameState));
+
+        // Recalculate
+        GoActionGo();
+    });
+
+
+    // For the milk
+    MilkEvent();
+
+    // For the flavors
+    FlavorsEvent();
+
+    // For the prodUpgrade
+    prodUpgradeEvent();
+
+    // For the kittens
+    KittenEvent();
+
+
+    
+}
+
+function BuildingsEvent() {
     for (let buildingKey in gameState.buildings) {
         let building = gameState.buildings[buildingKey];
 
@@ -258,7 +372,7 @@ function addEventListeners() {
             grandmaBoostInput.addEventListener('change', () => {
                 // Update grandmaBoosted in gameState
                 gameState.buildings[buildingKey].grandmaBoosted = grandmaBoostInput.checked;
-                
+
                 // Save to local storage
                 localStorage.setItem('gameState', JSON.stringify(gameState));
 
@@ -266,9 +380,49 @@ function addEventListeners() {
                 GoActionGo();
             });
         }
-    }
 
-    // For the milk
+        // If the building is "Grandma", add the event listener for "grandmaGrandmaUpgrade"
+        if (building.name === 'Grandma') {
+            let grandmaGrandmaUpgradeInput = document.getElementById('grandmaGrandmaUpgrade');
+            let buildingNamesSelect = document.getElementById('building-names');
+            if (grandmaGrandmaUpgradeInput) {
+                grandmaGrandmaUpgradeInput.addEventListener('keyup', (event) => {
+                    // Check for 'Enter' key
+                    if (event.key === 'Enter') {
+                        // Update gameState
+                        gameState.buildings[buildingKey].upgradeGrandmaCost = parseInt(grandmaGrandmaUpgradeInput.value, 10);
+                        grandmaGrandmaUpgradeInput.value = '';
+
+                        // Save to local storage
+                        localStorage.setItem('gameState', JSON.stringify(gameState));
+
+                        // Recalculate
+                        GoActionGo();
+
+                        // Lose focus from the input
+                        event.target.blur();
+                    }
+                });
+            }
+
+            // Add event listener for 'change' event on buildingNamesSelect
+            if (buildingNamesSelect) {
+                buildingNamesSelect.addEventListener('change', () => {
+                    // Update upgradeGrandmaType in gameState
+                    gameState.buildings[buildingKey].upgradeGrandmaType = buildingNamesSelect.value;
+
+                    // Save to local storage
+                    localStorage.setItem('gameState', JSON.stringify(gameState));
+
+                    // Recalculate
+                    GoActionGo();
+                });
+            }
+        }
+    }
+}
+
+function MilkEvent() {
     let milkInput = document.getElementById("milkInput");
     if (milkInput) {
         milkInput.addEventListener('keyup', (event) => {
@@ -281,9 +435,10 @@ function addEventListeners() {
             }
         });
     }
+}
 
-    // For the flavors
-    for(let i = 1; i <= 10; i += (i < 6) ? 1 : 5) {
+function FlavorsEvent() {
+    for (let i = 1; i <= 10; i += (i < 6) ? 1 : 5) {
         let flavorInput = document.getElementById(`input${i}`);
         if (flavorInput) {
             flavorInput.addEventListener('keyup', (event) => {
@@ -297,50 +452,49 @@ function addEventListeners() {
             });
         }
     }
-
-// For the prodUpgrade
-let prodUpgradeInput = document.getElementById("prodUpgrade");
-if (prodUpgradeInput) {
-    prodUpgradeInput.addEventListener('keyup', (event) => {
-        // Check for 'Enter' key
-        if (event.key === 'Enter') {
-            gameState.prodBoosts.prodUpgrade.upgradeCost = parseInt(prodUpgradeInput.value, 10);
-            prodUpgradeInput.value = '';
-
-            // Check which radio button is checked
-            for (let i = 1; i <= 5; i++) {
-                let radioInput = document.getElementById(`flavor${i}`);
-                let flavor = radioInput.value;
-                gameState.prodBoosts.prodUpgrade.upgradePercents[flavor][0] = radioInput.checked;
-            }
-
-            localStorage.setItem('gameState', JSON.stringify(gameState));
-            GoActionGo();
-            event.target.blur();
-        }
-    });
 }
 
-// For the kittens
-let kittenTypes = ['helpers', 'workers', 'engineers', 'overseers'];
-kittenTypes.forEach(kittenType => {
-    let kittenInput = document.getElementById(`kitten-${kittenType}`);
-    if (kittenInput) {
-        kittenInput.addEventListener('change', (event) => {
-            // Update gameState
-            gameState.prodBoosts.kittens[kittenType][0] = event.target.checked;
+function prodUpgradeEvent() {
+    let prodUpgradeInput = document.getElementById("prodUpgrade");
+    if (prodUpgradeInput) {
+        prodUpgradeInput.addEventListener('keyup', (event) => {
+            // Check for 'Enter' key
+            if (event.key === 'Enter') {
+                gameState.prodBoosts.prodUpgrade.upgradeCost = parseInt(prodUpgradeInput.value, 10);
+                prodUpgradeInput.value = '';
 
-            // Save to local storage
-            localStorage.setItem('gameState', JSON.stringify(gameState));
+                // Check which radio button is checked
+                for (let i = 1; i <= 5; i++) {
+                    let radioInput = document.getElementById(`flavor${i}`);
+                    let flavor = radioInput.value;
+                    gameState.prodBoosts.prodUpgrade.upgradePercents[flavor][0] = radioInput.checked;
+                }
 
-            // Recalculate
-            GoActionGo();
+                localStorage.setItem('gameState', JSON.stringify(gameState));
+                GoActionGo();
+                event.target.blur();
+            }
         });
     }
-});
+}
 
+function KittenEvent() {
+    let kittenTypes = ['helpers', 'workers', 'engineers', 'overseers'];
+    kittenTypes.forEach(kittenType => {
+        let kittenInput = document.getElementById(`kitten-${kittenType}`);
+        if (kittenInput) {
+            kittenInput.addEventListener('change', (event) => {
+                // Update gameState
+                gameState.prodBoosts.kittens[kittenType][0] = event.target.checked;
 
-    
+                // Save to local storage
+                localStorage.setItem('gameState', JSON.stringify(gameState));
+
+                // Recalculate
+                GoActionGo();
+            });
+        }
+    });
 }
 
 function formatValue(num, decimals = 2, isCost = false) {
@@ -369,6 +523,7 @@ function GoActionGo() {
     totalcps = 0;
     prodUpgradeEfficiency = 0;
     grandmaTotal = 0;
+    nonCursorBuildingTotal = 0;
 
     CalcGlobalBoost();
     AdjustBuildings();
